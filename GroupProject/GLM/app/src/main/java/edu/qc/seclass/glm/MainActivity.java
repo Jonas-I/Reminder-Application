e8c57383c4224f355e1dabcb872862cb1a67e9b6
Daniel Rand <Daniel.Rand56@qmail.cuny.edu>
2019-11-16 17:54:04 -0500

    notif stuff

--- GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/MainActivity.java
+++ GroupProject/GLM/app/src/main/java/edu/qc/seclass/glm/MainActivity.java
@@ -1,9 +1,12 @@
 package edu.qc.seclass.glm;
 
 import android.app.AlarmManager;
+import android.app.Notification;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ExpandableListView;
@@ -13,7 +16,6 @@ import android.widget.Toast;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -64,13 +66,7 @@ public class MainActivity extends AppCompatActivity {
         if (!areNotificationsEnabled)
             enableNotifications();
 
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.MINUTE, calendar.get(Calendar.MINUTE)+2);
-        calendar.set(Calendar.MINUTE, calendar.get(Calendar.MINUTE)+2);
-        Intent intent1 = new Intent(MainActivity.this, AlarmReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0,intent1, PendingIntent.FLAG_UPDATE_CURRENT);
-        AlarmManager am = (AlarmManager) MainActivity.this.getSystemService(MainActivity.this.ALARM_SERVICE);
-        am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+        scheduleNotification(getNotification("10 second delay"), 10000);
 
 
         createButton = findViewById(R.id.createButton);
@@ -176,4 +172,25 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("android.provider.extra.APP_PACKAGE", getPackageName());
         startActivity(intent);
     }
+
+    private void scheduleNotification(Notification notification, int delay) {
+
+        Intent notificationIntent = new Intent(this, NotificationPublisher.class);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+    }
+
+    private Notification getNotification(String content) {
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("Scheduled Notification");
+        builder.setContentText(content);
+        builder.setSmallIcon(R.drawable.icon);
+        return builder.build();
+    }
+
 }
